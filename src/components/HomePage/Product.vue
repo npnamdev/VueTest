d591e2c1fa8b085949b158a844b602d3e5d33281
Phuong Nam <npnam031101@gmail.com>
2023-03-28 10:59:38 -0500

    test

---
+++ src/components/HomePage/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="wp-content">
+    <h3>Sản Phẩm</h3>
+    <div id="main-product">
+      <li v-for="product in $store.state.products" :key="product.id">
+        <a href="#">
+          <div class="image">100x100</div>
+          <h5>{{ product.name }}</h5>
+          <span>{{ product.price }}$</span>
+          <button @click="$store.commit('addToCart', product)">
+            Add to cart
+          </button>
+        </a>
+      </li>
+    </div>
+    <br />
+    <div>{{ $store.state.cartProducts }}</div>
+  </div>
+</template>
+
